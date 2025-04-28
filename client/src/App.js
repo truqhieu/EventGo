1480ed0d73e782381163d26be25956447ebb6361
Danny <datlacve2@gmail.com>
2025-04-28 09:59:04 +0700

    fix staff

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Home, Public, Login } from "./pages/public";
 import path from "./ultils/path";
 import { Speaker, Event, DetailEvent } from "./components";
 import FinalRegister from "./pages/guest/FinalRegister";
-import { Admin } from "./pages/private";
+import { Admin, Staff } from "./pages/private";
 import About from "./components/About/About"
 import Contact from "./components/Contact/Contact"
 import EventRegistered from "./components/Event/EventRegistered";
@@ -25,6 +25,7 @@ function App() {
         <Route path={path.LOGIN} element={<Login />} />
         <Route path={path.FinalRegister} element={<FinalRegister />} />
         <Route path={path.Admin} element={<Admin />} />
+        <Route path={path.Staff} element={<Staff />} />
       </Routes>
     </div>
   );
